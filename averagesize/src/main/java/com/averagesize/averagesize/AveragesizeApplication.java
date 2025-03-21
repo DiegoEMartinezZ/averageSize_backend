c20be4eb4c784aa83a84fea3cc4cf7ff2a32f9fd
DiegoEMartinezZ <diermaza.23@gmail.com>
2025-03-21 10:38:05 -0500

    Adding the Spring/Maven project to the remote repository

---
+++ averagesize/src/main/java/com/averagesize/averagesize/AveragesizeApplication.java
@@ -0,0 +1,13 @@
+package com.averagesize.averagesize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AveragesizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AveragesizeApplication.class, args);
+	}
+
+}
---
+++ averagesize/src/test/java/com/averagesize/averagesize/AveragesizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.averagesize.averagesize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AveragesizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
