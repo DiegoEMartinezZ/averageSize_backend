cb8d530734ffb4c47dc479c2b5085aec89f0c79b
DiegoEMartinezZ <diermaza.23@gmail.com>
2025-03-23 12:19:55 -0500

    fix some mistakes inside the entities

--- averagesize/src/main/java/com/averagesize/averagesize/entity/Link.java
+++ averagesize/src/main/java/com/averagesize/averagesize/entity/Link.java
@@ -1,6 +1,5 @@
 package com.averagesize.averagesize.entity;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.*;
--- averagesize/src/main/java/com/averagesize/averagesize/entity/User.java
+++ averagesize/src/main/java/com/averagesize/averagesize/entity/User.java
@@ -1,6 +1,5 @@
 package com.averagesize.averagesize.entity;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
